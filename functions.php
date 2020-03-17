542f040d380d7dd2a462f2fddbadc539798229d1
viergenie <viergeniejl29@gmail.com>
2020-03-17 12:21:49 -0300

    restart the projet

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+// Ajouter automatiquement le titre du site dans l'en-tête du site
+add_theme_support( 'title-tag' );
+/*** Ajouter un script ou un fichier CSS de la bonne façon */
+function wpdocs_theme_name_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'etan_webflow', get_template_directory_uri().'/css/etan.webflow.css' );
+    wp_enqueue_style( 'normalize', get_template_directory_uri().'/css/normalize.css' );
+    wp_enqueue_style( 'webflow', get_template_directory_uri().'/css/webflow.css' );
+    wp_enqueue_script( 'webflow', get_template_directory_uri().'/js/webflow.js' );
+}
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+//add_action( 'wp_head', 'my_header_scripts' );
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <?php get_header() ?>
+        <title><?php bloginfo('name'); ?></title>
+    </head>
+<body>
+     <?php while(have_posts()) : the_post(); ?>
+
+<h1><?php the_title(); ?></h1>
+  <?php the_content(); ?>
+
+   <?php endwhile; ?>
+</body>
+</html>
